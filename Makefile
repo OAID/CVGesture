dec00dc881c1643df64d405642cbb779c1c22bf9
HaoHan <hanhao@openailab.com>
2018-01-05 16:45:14 +0800

    put config parameters into json file

--- Makefile
+++ Makefile
@@ -5,11 +5,14 @@ export PKG_CONFIG_PATH=/usr/local/AID/pkgconfig
 CFLAGS+= `pkg-config --cflags opencv`
 LDFLAGS+= `pkg-config --libs opencv`
 
-demo: demo.o
-	$(CXX) -O3 -o demo demo.o $(LDFLAGS)
+demo: demo.o json.o
+	$(CXX) -O3 -o demo demo.o json.o $(LDFLAGS)
 
 demo.o: demo.cpp
 	$(CXX) -O3 -c demo.cpp ${CFLAGS}
+	
+json.o: json.cpp
+	$(CXX) -O3 -c json.cpp ${CFLAGS}
 	
 clean: 
 	rm -f *.d *.o
--- demo.cpp
+++ demo.cpp
@@ -6,10 +6,57 @@
 #include <stdlib.h> 
 #include <cstdlib>
 #include <list>
+#include <string>
+#include <fstream>
+#include <memory.h>
+#include "json/json.h"
+
+#define CONFIG_FILENAME "config.json"
 
 void DrawText(cv::Mat& img,std::string text,int x, int y,cv::Scalar color)
 {
     cv::putText(img,text.c_str(),cv::Point(x,y),cv::FONT_HERSHEY_SIMPLEX,0.8,color,2,1);
+}
+
+struct sys_config
+{
+    int palm_detect_neighbor_num;
+    int palm_detect_size;
+    int fist_detect_neighbor_num;
+    int fist_detect_size;
+    int iter_time;
+};
+
+bool parse_config(char * path, sys_config & config)
+{
+    std::ifstream ifs;
+    Json::Reader reader;
+    Json::Value root;
+
+    ifs.open(path, std::ios::in | std::ios::binary);
+    if (ifs.is_open() == false) {
+		std::cout << "Open file failed!\n";
+        return false;
+    }
+
+    if (!reader.parse(ifs, root, false)) {
+		std::cout << "Read file failed!\n";
+        ifs.close();
+        return false;
+    }
+
+    memset(&config, 0, sizeof(sys_config));
+
+    config.palm_detect_neighbor_num = root["palm detect neighbor number"].asInt();
+    config.palm_detect_size = root["palm detect rectangle size"].asInt();
+
+    config.fist_detect_neighbor_num = root["fist detect neighbor number"].asInt();
+    config.fist_detect_size = root["fist detect rectangle size"].asInt();
+
+    config.iter_time = root["test iteration time"].asInt();
+
+    ifs.close();
+        return true;
 }
 
 int main()
@@ -24,29 +71,47 @@ int main()
     Fist.load("fist_v3.xml");
     Palm.load("palm_v4.xml");
 
+    sys_config config;
+    
+
     double time_past=0;
     int frame_cnt = 0;
     int T=5;
     double avg_fps=0;
     int64 s;
     double time_palm=0, time_fist=0;
+    int pdetect_num, fdetect_num;
+    int pdetect_rec, fdetect_rec;
+    
+    if(parse_config((char *)CONFIG_FILENAME, config))
+    {
+        pdetect_num = config.palm_detect_neighbor_num;
+        pdetect_rec = config.palm_detect_size;
+        fdetect_num = config.fist_detect_neighbor_num;
+        fdetect_rec = config.fist_detect_size;
+        T = config.iter_time;
+    }
+    else
+    {
+        std::cout << "config error!" << std::endl;
+    }
 
     while(true)
     {
         cap>>img;
 	int64 start = cv::getTickCount();
 	s = cv::getTickCount();
-        Palm.detectMultiScale(img,prect,1.1,7,0|CV_HAAR_SCALE_IMAGE,cv::Size(90,90));
+        Palm.detectMultiScale(img,prect,1.1,pdetect_num,0|CV_HAAR_SCALE_IMAGE,cv::Size(pdetect_rec,pdetect_rec));
 	time_palm += (cv::getTickCount() - s) / cv::getTickFrequency();
-	for (unsigned int i = 0; i < prect.size();i++){
+	for (int i = 0; i < prect.size();i++){
 	    cv::rectangle(img, prect[i],cv::Scalar(0,0,255),4); 
 	    cv::putText(img,"Palm",cv::Point(prect[i].x,prect[i].y-10),cv::FONT_HERSHEY_SIMPLEX,0.8,cv::Scalar(0,0,255),2,1);	
 	
 	}
 	s = cv::getTickCount();
-	Fist.detectMultiScale(img,frect,1.1,7,0|CV_HAAR_SCALE_IMAGE,cv::Size(80,80));
+	Fist.detectMultiScale(img,frect,1.1,fdetect_num,0|CV_HAAR_SCALE_IMAGE,cv::Size(fdetect_rec,fdetect_rec));
 	time_fist += (cv::getTickCount() - s) / cv::getTickFrequency();
-	for (unsigned int i = 0; i < frect.size();i++){
+	for (int i = 0; i < frect.size();i++){
 	    cv::rectangle(img, frect[i],cv::Scalar(0,255,0),4);
 	    cv::putText(img,"Fist",cv::Point(frect[i].x,frect[i].y-10),cv::FONT_HERSHEY_SIMPLEX,0.8,cv::Scalar(0,255,0),2,1);	
 	}
